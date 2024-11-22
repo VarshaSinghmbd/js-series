95550eebcfeba3fc4a6eadf4080c7c0ad7dece0f
Varsha Singh <108179289+VarshaSinghmbd@users.noreply.github.com>
2024-11-22 13:54:36 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 25519
+let accountEmail = "varsha@google.com"
+var accountPasssword = "Singh000"
+accountCity = "gurgaon"
+let accountState;
+
+// const not a change
+
+accountEmail = "varshasingh1082@gmail.com"
+accountPasssword ="school9090"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPasssword);
+
+console.table([accountId, accountEmail, accountPasssword, accountCity,accountState])
+
+/*
+prefer not to use var
+becouse of issue in block scope and functional scope
+*/
