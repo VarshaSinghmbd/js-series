04c67886e931ba32a3f5b3ae3da2e98a94282871
Varsha Singh <108179289+VarshaSinghmbd@users.noreply.github.com>
2024-11-22 13:52:44 +0000

    datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+//alert (3+3)  //we are using nodeJS, not brower
+
+let name = "varsha"
+let age = 21
+let isLoiggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof age); //number
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
